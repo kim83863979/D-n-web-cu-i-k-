114439ecb856e2bba3f68730ae0519dfe487e1fd
Huỳnh Nguyễn Bảo Long <longhnb2692@ut.edu.vn>
2026-04-18 07:53:36 +0700

    Update footer contact information and developer name

--- LifestyleStore/logout.php
+++ LifestyleStore/logout.php
@@ -40,8 +40,8 @@
             <footer class="footer">
                <div class="container">
                <center>
-                   <p>Copyright &copy Lifestyle Store. All Rights Reserved. | Contact Us: +91 90000 00000</p>
-                   <p>This website is developed by Sajal Agrawal</p>
+                   <p>Copyright &copy Lifestyle Store. All Rights Reserved. | Contact Us: +84 00000 00000</p>
+                   <p>This website is developed by Nhom 6 Lap Trinh Web</p>
                </center>
                </div>
            </footer>
