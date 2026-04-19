7d1d79be46c14bd73b57ccc0d7e88bf4f6372739
ngoc100106 <tathinhungoc10012006@gmail.com>
2026-04-19 11:49:37 +0700

    lưu thuộc tính Size

--- LifestyleStore/cart_add.php
+++ LifestyleStore/cart_add.php
@@ -4,7 +4,8 @@
     session_start();
     $item_id=$_GET['id'];
     $user_id=$_SESSION['id'];
-    $add_to_cart_query="insert into users_items(user_id,item_id,status) values ('$user_id','$item_id','Added to cart')";
+    $size = $_GET['size'];
+    $add_to_cart_query="insert into users_items(user_id,item_id,size,status) values ('$user_id','$item_id','$size','Added to cart')";
     $add_to_cart_result=mysqli_query($con,$add_to_cart_query) or die(mysqli_error($con));
     header('location: products.php');
 ?>
