286362b536876df21924d946e69000dad146f7e8
kim83863979 <kimlq2957@ut.edu.vn>
2026-04-19 10:17:15 +0700

    Lấy quyền từ tài khoản người dùng nhập vào
    Added role-based redirection after user login.

--- LifestyleStore/login_submit.php
+++ LifestyleStore/login_submit.php
@@ -19,10 +19,17 @@
     $user_authentication_query="select id,email from users where email='$email' and password='$password'";
     $user_authentication_result=mysqli_query($con,$user_authentication_query) or die(mysqli_error($con));
     $rows_fetched=mysqli_num_rows($user_authentication_result);
+
+    // Phân quyền
+    // Truy vấn thêm cột role
+    $user_authentication_query = "SELECT id, email, role FROM users WHERE email='$email' AND password='$password'";
+    $user_authentication_result = mysqli_query($con, $user_authentication_query) or die(mysqli_error($con));
+    $rows_fetched = mysqli_num_rows($user_authentication_result);
+
     if($rows_fetched==0){
         //no user
         //redirecting to same login page
-        ?>
+        ?> 
         <script>
             window.alert("Wrong username or password");
         </script>
@@ -34,7 +41,17 @@
         $row=mysqli_fetch_array($user_authentication_result);
         $_SESSION['email']=$email;
         $_SESSION['id']=$row['id'];  //user id
-        header('location: products.php');
+        $_SESSION['role'] = $row['role']; // LƯU THÊM QUYỀN VÀO SESSION
+
+        // Điều kiện nhảy trang
+        if($_SESSION['role'] == 1) {
+            // Là Admin -> Bay vào trang quản trị
+            header('location: admin_dashboard.php');
+            exit();
+        } else {
+            // Là Khách hàng -> Chuyển về trang mua sắm
+            header('location: products.php');
+            exit();
+        }
     }
-    
  ?>
